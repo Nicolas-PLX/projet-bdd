fadb6a6891e3f1add6fda637844b68705143f720
FLICI yacine <flici@informatique.univ-paris-diderot.fr>
2023-05-07 13:37:48 +0200

    Correction creation table

--- create_table.sql
+++ create_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE Utilisateurs
 (
     id_user SERIAL PRIMARY KEY,
-    pseudo VARCHAR(30) NOT NULL,
+    pseudo VARCHAR(30) NOT NULL CHECK (LENGTH(pseudo) <= 20),
     email varchar(30) NOT NULL,
-    pwd VARCHAR(30) NOT NULL CHECK (LENGTH(pwd) >= 8 AND LENGTH(nom) <= 20),
+    pwd VARCHAR(30) NOT NULL CHECK (LENGTH(pwd) >= 8),
     pays VARCHAR(30),
     ville VARCHAR(30),
     date_creation DATE NOT NULL
@@ -12,15 +12,13 @@ CREATE TABLE Utilisateurs
 CREATE TABLE Personnes
 (
     id INTEGER PRIMARY KEY,
-   date_naissance DATE,
-   FOREIGN KEY(id) REFERENCES Utilisateurs(id) 
+    date_naissance DATE
 ) INHERITS(Utilisateurs);
 
 CREATE TABLE Groupe
 (
     id_groupe INTEGER PRIMARY KEY,
-    nom VARCHAR(30) NOT NULL,
-    FOREIGN KEY(id_groupe) REFERENCES Utilisateurs(id)
+    nom VARCHAR(30) NOT NULL
 ) INHERITS(Utilisateurs);
 
 CREATE TABLE Amis
@@ -59,22 +57,20 @@ CREATE TABLE Organisation
     id_concert INTEGER NOT NULL,
     PRIMARY KEY(id_user,id_concert),
     FOREIGN KEY(id_user) REFERENCES Utilisateurs(id_user),
-    FOREIGN KEY(id_concert) REFERENCE Concert(id_concert)
+    FOREIGN KEY(id_concert) REFERENCES Concert(id_concert)
 );
 
 
 CREATE TABLE Concert_prevu
 (
     id_concert INTEGER PRIMARY KEY,
-    nb_places_restantes INTEGER NOT NULL,
-    FOREIGN KEY(id_concert) REFERENCES Concert(id_concert)
+    nb_places_restantes INTEGER NOT NULL
 )INHERITS(Concert);
 
 CREATE TABLE Concert_fini
 (
-    id_concert INTEGER NOT NULL,
-    PRIMARY KEY(id_concert,id_archive),
-    FOREIGN KEY(id_concert) REFERENCES Concert(id_concert),
+    id_archive INTEGER NOT NULL,
+    PRIMARY KEY(id_concert,id_archive)
 )INHERITS(Concert);
 
 CREATE TABLE Archive
@@ -84,7 +80,7 @@ CREATE TABLE Archive
     nb_participant INTEGER NOT NULL,
     photo VARCHAR(50),
     video VARCHAR(50),
-    FOREIGN KEY(id_concert) REFERENCES Concert_fini(id_concert)
+    FOREIGN KEY(id_concert) REFERENCES Concert(id_concert)
 );
 
 CREATE TABLE Lieu
@@ -93,13 +89,13 @@ CREATE TABLE Lieu
     adresse VARCHAR(50) NOT NULL,
     ville VARCHAR(50) NOT NULL,
     code_postal VARCHAR(10) NOT NULL,
-    pays VARCHAR(20) NOT NULL,
+    pays VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Lieu_concert
 (
     id_concert INTEGER NOT NULL,
-    id_lieu INTEGER NOT NULL
+    id_lieu INTEGER NOT NULL,
     PRIMARY KEY(id_concert,id_lieu),
     FOREIGN KEY(id_concert) REFERENCES Concert(id_concert),
     FOREIGN KEY (id_lieu) REFERENCES Lieu(id_lieu) 
@@ -112,12 +108,13 @@ CREATE TABLE Artiste
     id_groupe INTEGER,
     FOREIGN KEY(id_groupe) REFERENCES Groupe(id_groupe)
 );
+-- CREATE TYPE type_avis AS ENUM ('Morceau', 'Artiste', 'Lieu', 'Concert');
 
 CREATE TABLE Avis
 (
     id_avis SERIAL PRIMARY KEY,
     id_type INTEGER NOT NULL,
-    type_avis ENUM('Morceau','Artiste','Lieu','Concert') NOT NULL,
+    type_avis  type_avis NOT NULL,
     note INTEGER NOT NULL CHECK (note >= 0 AND note <= 10),
     commentaire VARCHAR(140),
     date_avis DATE NOT NULL
@@ -129,7 +126,7 @@ CREATE TABLE Archive_avis
     id_archive INTEGER NOT NULL,
     PRIMARY KEY(id_avis,id_archive),
     FOREIGN KEY(id_avis) REFERENCES Avis(id_avis),
-    FOREIGN KEY(id_archive) REFERENCES Utilisateurs(id_archive)   
+    FOREIGN KEY(id_archive) REFERENCES Archive(id_archive)   
 );
 
 CREATE TABLE Auteur_avis
@@ -184,6 +181,7 @@ CREATE TABLE Relation_genre
     FOREIGN KEY(id_parent) REFERENCES Genre(id_genre),
     FOREIGN KEY(id_fils) REFERENCES Genre(id_genre)
 );
+-- CREATE TYPE type_tag AS ENUM('Groupe','Concert','Lieu','Playlist') ;
 
 CREATE TABLE Tag
 (
---
+++ drop.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS
+    Relation_genre, Genre, Contient, Morceau, Playlist,
+    Auteur_avis, Archive_avis, Avis, Artiste,
+    Lieu_concert, Lieu, Archive, Concert_fini, Concert_prevu,
+    Organisation, Participation, Concert, Amis,
+    Groupe, Personnes, Utilisateurs;
