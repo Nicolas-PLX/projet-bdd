d52e4d63460366e5d4f12a6c1779f55af5d08ba9
Nicolas PENELOUX <nicolas.peneloux@free.fr>
2023-05-26 01:40:13 +0200

    ajout requete

--- Requete_sql.sql
+++ Requete_sql.sql
@@ -118,6 +118,7 @@ HAVING COUNT(Avis.id_avis) > 0;
 SELECT u.pseudo,u.id_user
 FROM Utilisateurs u
 JOIN Participation p ON u.id_user = p.id_personne
+WHERE p.a_participe = True
 GROUP BY u.id_user
 HAVING COUNT(p.id_concert) > 0
 ORDER BY u.id_user ASC;
@@ -127,26 +128,17 @@ SELECT u.pseudo,u.id_user
 FROM Utilisateurs u
 WHERE u.id_user IN (SELECT u.id_user 
                   FROM Participation p 
-                  WHERE u.id_user = p.id_personne);
+                  WHERE u.id_user = p.id_personne AND p.a_participe = True);
 
 --Requête récursive : pas sûr du tout mais bon
 
 \! echo "Requête 13 : Requête récursive : arbre généalogiques des genres "
-/*
-WITH RECURSIVE Arborescence_Genres AS (
-    SELECT id_genre, nom, ARRAY[id_genre] AS chemin
-    FROM Genre
-    WHERE id_genre = 1 -- Genre racine de départ
-    UNION ALL
-    SELECT g.id_genre, g.nom, ag.chemin || g.id_genre
-    FROM Genre g
-    INNER JOIN Arborescence_Genres ag ON g.id_genre = ANY(ag.chemin) -- Jointure récursive avec les genres parents déjà sélectionnés
-)
-SELECT id_genre, nom, chemin
-FROM Arborescence_Genres
-ORDER BY chemin;
+SELECT a.nom AS nom_artiste,COUNT(DISTINCT l.id_concert) AS nombre_concerts
+FROM Artiste a
+JOIN Lineup l ON a.id_artiste = l.id_artiste
+GROUP BY a.id_artiste, a.nom
+ORDER BY COUNT(DISTINCT l.id_concert) DESC;
 
-*/
 -- Requête avec fenêtrage
 
 \! echo "Requête 14 : Classement des utilisateurs en fonction de leur participation à des concerts:";
@@ -163,7 +155,86 @@ FROM (
     HAVING COUNT(p.id_personne) > 0
 ) AS subquery;
 
-\! echo "Requête 15 :"
+\! echo "Requête 15 : Récupère les utilisateurs qui ont participé à tout les concerts organisé dans une ville donnée :";
+
+\prompt 'Tapez le nom de la ville -> ' v_ville
+SELECT u.id_user, u.pseudo
+FROM Utilisateurs u
+JOIN Participation p ON u.id_user = p.id_personne
+JOIN Concert c ON p.id_concert = c.id_concert
+JOIN Lieu_concert lc ON lc.id_concert = c.id_concert
+JOIN Lieu l ON l.id_lieu = lc.id_lieu
+WHERE l.ville = :'v_ville'
+GROUP BY u.id_user, u.pseudo
+HAVING COUNT(DISTINCT c.id_concert) = (
+    SELECT COUNT(*)
+    FROM Concert JOIN Lieu_concert ON Lieu_concert.id_concert = Concert.id_concert
+    JOIN Lieu ON Lieu.id_lieu = Lieu_concert.id_lieu
+    WHERE ville = :'v_ville'
+);
 
 
+\! echo "Requête 16 : Calcule de la différence de prix entre chaque concert et le concert précédent dans une même ville :";
+SELECT c.id_concert, c.nom, c.date_concert, l.ville, c.prix,
+       prix - LAG(prix) OVER (PARTITION BY ville ORDER BY date_concert) AS diff_prix
+FROM Concert c JOIN Lieu_concert lc ON c.id_concert = lc.id_concert
+JOIN Lieu l ON lc.id_lieu = l.id_lieu
+ORDER BY ville, date_concert;
 
+\! echo "Requête 17 :La liste des villes qui ont une moyenne de prix de concert supérieur à la moyenne générale :";
+
+WITH avg_prices AS (
+    SELECT l.ville, AVG(prix) AS moyenne_prix
+    FROM Concert c JOIN Lieu_concert lc ON lc.id_concert = c.id_concert
+    JOIN Lieu l ON l.id_lieu = lc.id_lieu
+    GROUP BY l.ville
+), overall_avg AS (
+    SELECT AVG(prix) AS moyenne_generale
+    FROM Concert
+)
+SELECT a.ville, a.moyenne_prix
+FROM avg_prices a
+JOIN overall_avg o ON a.moyenne_prix > o.moyenne_generale
+ORDER BY a.moyenne_prix DESC;
+
+\i echo "Requête 18 : La liste des utilisateurs ayant des morceaux en commun dans leur playlist, ainsi que le nombre de morceaux en commun :";
+
+SELECT DISTINCT p1.id_user AS utilisateur_1, p2.id_user AS utilisateur_2, COUNT(*) AS nombre_morceaux_communs
+FROM Playlist p1
+JOIN Contient c1 ON p1.id_playlist = c1.id_playlist
+JOIN Contient c2 ON c1.id_morceau = c2.id_morceau
+JOIN Playlist p2 ON c2.id_playlist = p2.id_playlist
+WHERE p1.id_user < p2.id_user 
+GROUP BY p1.id_user, p2.id_user;
+
+\i echo "Requête 19 : Requête récursive qui va récupérer la liste de tous les utilisateurs qui sont suivis par un utilisateur donné";
+
+
+\prompt 'Tapez l id de l utilisateur que vous souhaitez voir ->' c_user
+WITH RECURSIVE SuivisUtilisateur(suiveur, suivi) AS (
+  SELECT suiveur, suivi
+  FROM Suivis
+  WHERE suiveur = :c_user
+  UNION ALL
+  SELECT Suivis.suiveur, Suivis.suivi
+  FROM SuivisUtilisateur
+  JOIN Suivis ON SuivisUtilisateur.suivi = Suivis.suiveur
+)
+SELECT DISTINCT suivi
+FROM SuivisUtilisateur;
+
+\i echo "Requête 20 : Requête récursive qui va récupérer tout les parents d'un genre donné :";
+
+\prompt 'Tapez l id du genre à celui dont vous souhaitez voir ses parents ->' c_genre
+WITH RECURSIVE GenresParents(id_genre, id_parent) AS (
+  SELECT id_fils, id_parent
+  FROM Relation_genre
+  WHERE id_fils = :c_genre
+  UNION ALL
+  SELECT Relation_genre.id_fils, Relation_genre.id_parent
+  FROM GenresParents
+  JOIN Relation_genre ON GenresParents.id_parent = Relation_genre.id_fils
+)
+SELECT id_parent
+FROM GenresParents 
+ORDER BY id_parent DESC;
